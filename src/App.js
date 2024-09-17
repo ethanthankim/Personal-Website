890e255bd335344bd01e92139b98e978bf248179
ethanthankim <ethan.kim1996@gmail.com>
2024-09-17 18:15:31 -0400

    misc

--- src/App.js
+++ src/App.js
@@ -10,6 +10,10 @@ import {
 
 // import components
 import Home from "./components/Home"
+import About from "./components/About"
+import Projects from "./components/Projects"
+import Videos from "./components/Videos"
+import Contact from "./components/Contact"
 
 function App() {
   return (
@@ -24,6 +28,22 @@ function App() {
           exact
           path="/Home"
           element={<Home />}
+        />
+        <Route
+          path="/About"
+          element={<About />}
+        />
+        <Route 
+          path="/Projects"
+          element={<Projects />}
+        />
+        <Route 
+          path="/Videos"
+          element={<Videos />}
+        />
+        <Route 
+          path="/Contact"
+          element={<Contact />}
         />
         <Route
           path="*"
